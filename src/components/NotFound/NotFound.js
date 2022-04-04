2ca8ef6719019a5596e2e4b3a31bdca45a3383fd
fahimabushra1 <bushra.rifeen@gmail.com>
2022-04-04 14:49:00 +0600

    404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className="text-center text-green-700 mt-32">
+            <h1 className="text-6xl">404</h1>
+            <p className="text-2xl">data not found</p>
         </div>
     )
 }
