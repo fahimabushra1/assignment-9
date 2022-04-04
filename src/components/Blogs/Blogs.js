6ba0a226739a9d7658ecdc8f727a257d6c93c9e4
fahimabushra1 <bushra.rifeen@gmail.com>
2022-04-04 21:54:27 +0600

    context-api

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,7 @@ const Blogs = () => {
     return (
         <div className="text-green-700 mt-12 ml-10">
             <h1 className="text-2xl">What is Context API?</h1>
-            <p className="text-xl mt-4">Context API way that produce global</p>
+            <p className="text-xl mt-4">Context API is a new feature of React.It is a way to produce global variables for React app that can be passed around.This is the alternative  to move prop from grandparent to child.It passes data through components without passing props.One can easily share state across the entire app.</p>
         </div>
     )
 }
