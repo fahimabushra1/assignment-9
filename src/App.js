e3c25b925ccf6a068d59393d9dac7bb803595812
fahimabushra1 <bushra.rifeen@gmail.com>
2022-04-04 09:37:52 +0600

    import-Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
+import DashBoard from "./components/DashBoard/DashBoard";
+import Blogs from "./components/Blogs/Blogs";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/reviews' element={<Reviews />} />
+      <Route path='/dashboard' element={<DashBoard />}></Route>
+      <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      <Route path='/About' element={<About></About>}></Route>
+      <Route path='/*' element={<NotFound></NotFound>}></Route>
+    </Routes>
   );
 }
 
